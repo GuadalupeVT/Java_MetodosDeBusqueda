8ffc58da474c2bee56051c1b4ca2d508a592d025
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-29 08:12:58 -0600

    Se agrego el metodo de Busqueda secuencial en la clase MetodosDeBusqueda en PruebaMetodosDeBusqueda.java

--- src/PruebaMetodosDeBusqueda.java
+++ src/PruebaMetodosDeBusqueda.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 class MetodosDeBusqueda{
-	
+	public void BusquedaSecuencial (int []numeros) {
+		Scanner leer= new Scanner(System.in);
+		boolean existe=false;
+		
+		System.out.println("Ingrese el numero a buscar! ");
+		int numBuscado=leer.nextInt();
+		
+		for(int b=0; b<numeros.length;b++) {
+			if(numeros[b]==numBuscado) {
+				System.out.println("El numero si existe, en la posicion "+(b+1));
+				break;
+			}else {
+				existe=true;
+			}if(existe==true) {
+				System.out.println("El numero no existe");
+			}
+		}
+	}
 }
 
 public class PruebaMetodosDeBusqueda {
