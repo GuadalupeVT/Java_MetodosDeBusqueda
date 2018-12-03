b3c4302e7b6bd857f1fc5871e142869cba78e2ba
Guadalupe Vazquez <guvadlt@gmail.com>
2018-12-03 08:53:15 -0600

    se agrego el menu para busquedaBinaria

--- src/PruebaMetodosDeBusqueda.java
+++ src/PruebaMetodosDeBusqueda.java
@@ -2,6 +2,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 class MetodosDeBusqueda{
+	public static void ordenamientoQuickSort(int numeros[], int primero, int ultimo) {
+		int i,j,pivote,aux;
+		i=primero;
+		j=ultimo;
+		pivote=numeros[primero];
+		while(i<j) {
+			while(numeros[i]<=pivote && i<j) {
+				i++;
+			}
+			while(numeros[j]>pivote) {
+				j--;
+			}
+			if(i<j) {
+				aux=numeros[i];
+				numeros[i]=numeros[j];
+				numeros[j]=aux;
+			}
+		}
+		numeros[primero]=numeros[j];
+		numeros[j]=pivote;
+		if(primero<j-1) {
+			ordenamientoQuickSort(numeros, primero, j-1);
+		}
+		if(j+1<ultimo) {
+			ordenamientoQuickSort(numeros, j+1, ultimo);
+		}
+	}//Metodo ordenamientoQuickSort
 	public static void BusquedaSecuencial (int []numeros) {
 		Scanner leer= new Scanner(System.in);
 		boolean existe=false;
@@ -19,7 +46,8 @@ class MetodosDeBusqueda{
 		     if(existe==true) {
 				System.out.println("El numero no existe");
 		     }
-	}//BusquedaSecuencial
+	}//Busqueda secuencial
+	
 	public static int busquedaBinaria(int numeros[],int elemento) {
 		int centro,primero,valorCentro,ultimo;
 		primero=0;
@@ -27,7 +55,6 @@ class MetodosDeBusqueda{
 		while(primero <=ultimo) {
 			centro=(primero+ultimo)/2;
 			valorCentro=numeros[centro];
-			System.out.println("Comparando "+elemento+" con "+numeros[centro]);
 			if (elemento==valorCentro) {
 				return centro;
 			}else if(elemento<valorCentro) {
@@ -38,7 +65,8 @@ class MetodosDeBusqueda{
 		}
 		return -1;
 	}//BusquedaBinaria
-}
+	
+}//Class metodos de busqueda
 
 public class PruebaMetodosDeBusqueda {
 
@@ -53,15 +81,30 @@ public class PruebaMetodosDeBusqueda {
 		do {
 			System.out.println("________MENU________");
 			System.out.println("1. Busqueda Secuencial");
-			System.out.println("2. Salir");
+			System.out.println("2. Busqueda Binaria");
+			System.out.println("3. Salir");
 			menu=entrada.nextByte();
 			switch(menu) {
 			case 1: 
 				System.out.println(" ~~~~~~~~~Busqueda Secuencial~~~~~~~~~");
 				MetodosDeBusqueda.BusquedaSecuencial(arregloDesordenado.clone());
+				break;
+			case 2:
+				System.out.println(" ~~~~~~~~~Busqueda Binaria~~~~~~~~~");
+				int [] arr=arregloDesordenado.clone();
+				System.out.println("Ingrese el numero a buscar:");
+				int elemento=entrada.nextInt();
+				MetodosDeBusqueda.ordenamientoQuickSort(arr, 0, arr.length-1);
+				int a=MetodosDeBusqueda.busquedaBinaria(arr, elemento);
+				if(a!=1) {
+					System.out.println("Se encontro el elemento!");
+				}else {
+					System.out.println("No se encontro el elemento!");
+				}
+				break;
 			}
 			
-		}while(menu!=2);
+		}while(menu!=3);
 
 	}
 
